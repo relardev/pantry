a440c1531f33cc63c34a4c5662b59da56901e7aa
relar <relardev@gmail.com>
2024-11-24 19:51:18 +0100

    add origin

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,8 @@ import Config
 config :pantry, PantryWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "pantry.relar.dev"]
+  url: [host: "pantry.relar.dev"],
+  check_origin: ["//pantry.relar.dev"]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: Pantry.Finch
