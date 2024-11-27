63bea7cb0d804a90198122ac3100c582e6485a68
relar <relardev@gmail.com>
2024-11-27 20:53:36 +0100

    read url from env

--- config/prod.exs
+++ config/prod.exs
@@ -8,7 +8,6 @@ import Config
 config :pantry, PantryWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  url: [host: "pantry.relar.dev"],
   check_origin: ["//pantry.relar.dev"]
 
 # Disable Swoosh Local Memory Storage
